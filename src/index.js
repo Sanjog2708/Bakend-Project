1a31b89219378f31b2f1e3845447941a61a600b6
Sanjog Salunke <sanjog2708@gmail.com>
2024-12-28 08:42:25 +0530

    Utile Code Discussion

--- src/index.js
+++ src/index.js
@@ -5,11 +5,15 @@ import connectDB from "./db/index.js    ";
 dotenv.config({
     path: './env'
 })
-connectDB();
-
-
-
-
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at port ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("Mongo failed to connect !!!",err);
+})
 
 
 
---
+++ src/utils/ApiResponce.js
@@ -0,0 +1,10 @@
+class ApiResponce{
+    constructor(statusCode,data,message = 'Success')
+    {
+        this.statusCode = statusCode;
+        this.message = message;
+        this.data = data;
+        this.success = statusCode<400
+    }
+}
+export {ApiResponce}
